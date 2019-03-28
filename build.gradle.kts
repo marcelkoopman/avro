ae8927efaad64639a944f1e8b3165409b22aca20
marcelkoopman <marcel.koopman@gmail.com>
2019-03-28 20:40:47 +0100

    second commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     application
     kotlin("jvm") version "1.3.20"
+    java
 }
 
 application {
@@ -9,8 +10,6 @@ application {
 
 dependencies {
     compile(kotlin("stdlib"))
-    compile("org.apache.logging.log4j:log4j-slf4j-impl:2.11.2")
-    compile("org.apache.logging.log4j:log4j-core:2.11.2")
     compile("org.apache.avro:avro:1.8.2")
 }
 
@@ -26,10 +25,20 @@ buildscript {
         classpath("com.commercehub.gradle.plugin:gradle-avro-plugin:0.16.0")
     }
 }
+
 apply(plugin="com.commercehub.gradle.plugin.avro")
+
+apply(plugin="java")
+
+sourceSets.create("generated") {
+    java.setSrcDirs(listOf("src/main/java","src/gen/java").asIterable())
+}
 
 tasks.withType<com.commercehub.gradle.plugin.avro.GenerateAvroJavaTask>().configureEach{
     source("src/main/avro")
-    
-    setOutputDir(file("src/main/generated/"))
+    setOutputDir(file("src/gen/java"))
+}
+
+tasks.named<JavaCompile>("compileJava") {
+    source("src/gen/java")
 }
